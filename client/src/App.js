b00e70b13ef6df813c95b82ec9994bc19df8c98a
SUNDAY SAMUEL <orinamesunday360@gmail.com>
2022-07-18 06:49:21 -0700

    make it better

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import PortfolioContainer from "./PortfolioContainer/PortfolioContainer";
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ToastContainer />
+    <PortfolioContainer />
+    </div>
+  );
+}
+
+export default App;
